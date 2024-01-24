da1b9f8164c8c0a43e988eb084c2ac1c78d2e052
Josip Milan <josip.milan@aol.com>
2024-01-24 16:52:40 +0100

    fix index

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,12 +1,13 @@
-router.get('/', (req, res) => {
-  res.json('All good in here');
-})
+const router = require("express").Router();
 
-const artistRouter = require('./artist.route');
-router.use('/artist', artistRouter);
+router.get("/", (req, res) => {
+  res.json("All good in here");
+});
+
+const artistRouter = require("./artist.route");
+router.use("/artist", artistRouter);
 
 const genreRouter = require("./genre.routes");
 router.use("/genre", genreRouter);
-
 
 module.exports = router;
