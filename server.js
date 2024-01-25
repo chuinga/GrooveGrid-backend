e10d62210f3a49f1ae651fe9464f60e288fa435f
Josip Milan <josip.milan@aol.com>
2024-01-25 17:59:30 +0100

    changes of models

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
-const app = require('./app')
-const withDB = require('./db')
+const app = require("./app");
+const withDB = require("./db");
+const genres = require("./genressamples.json");
+const Genre = require("./models/Genre.model");
+
+const seed = async () => {
+  try {
+    const allGenres = await Genre.insertMany(genres);
+    console.log(allGenres);
+  } catch (error) {
+    console.log(error);
+  }
+};
+seed();
 
 // ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 5005
-const PORT = process.env.PORT || 5005
+const PORT = process.env.PORT || 5005;
 
 // ℹ️ Connects to the database
 withDB(() => {
   // ℹ️ If connection was successful, start listening for requests
   app.listen(PORT, () => {
-    console.log(`Server listening on http://localhost:${PORT}`)
-  })
-})
+    console.log(`Server listening on http://localhost:${PORT}`);
+  });
+});
