9fe07ce947558dc7b12a11733f878586b039a9f3
chuinga <mfproencamartins@gmail.com>
2024-01-25 10:20:48 +0000

    songs route added to index route

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -4,7 +4,7 @@ router.get("/", (req, res) => {
   res.json("All good in here");
 });
 
-const artistsRouter = require("../routes/artists.routes");
+const artistsRouter = require("./artists.routes");
 router.use("/artists", artistsRouter);
 
 const genreRouter = require("./genre.routes");
@@ -12,5 +12,8 @@ router.use("/genre", genreRouter);
 
 const albumRouter = require("./album.routes");
 router.use("/album", albumRouter);
+
+const songsRouter = require("./songs.routes");
+router.use("/songs", songsRouter);
 
 module.exports = router;
--- routes/songs.routes.js
+++ routes/songs.routes.js
@@ -5,63 +5,70 @@ const router = require('express').Router();
 router.post("/", async (req, res) => {
     const payload = req.body
     try {
-      const newSong = await Songs.create(payload);
-      res.status(201).json(newSong);
+        const newSong = await Songs.create(payload);
+        res.status(201).json(newSong);
     }
     catch (error) {
-      res.status(500).json({ message: 'Error creating a Song!!!' });
+        console.log(error);             
+        res.status(500).json({ message: 'Error creating a Song!!!' });
     }
 })
 
 // get all Songs
 router.get("/", async (req, res) => {
     try {
-      const allSongs = await Songs.find();
-      res.status(200).json(allSongs);
+        const allSongs = await Songs.find()
+        .populate('artist')
+        .populate('album')
+        .populate('genres');
+        res.status(200).json(allSongs);
     }
     catch (error) {
-      res.status(500).json({ message: 'Error getting all the Songs!!!' });
+        res.status(500).json({ message: 'Error getting all the Songs!!!' });
     }
-  })
+})
 
-  // get one Song
-  router.get("/:songId", async (req, res) => {
+// get one Song
+router.get("/:songId", async (req, res) => {
     try {
-      const songId = req.params.songId;
-      const oneSong = await Songs.findById(songId);
-      res.status(200).json(oneSong);
+        const songId = req.params.songId;
+        const oneSong = await Songs.findById(songId)
+        .populate('artist')
+        .populate('album')
+        .populate('genres');
+        res.status(200).json(oneSong);
     } catch (error) {
-      res.status(500).json({ message: `Error getting one Song` });
+        res.status(500).json({ message: `Error getting one Song` });
     }
-  });
+});
 
-  // update one Song
-  router.put("/:songId", async (req, res) => {
+// update one Song
+router.put("/:songId", async (req, res) => {
     try {
-      const songId = req.params.songId;
-      const songToUpdate = req.body;   
-      const updatedSong = await Songs.findByIdAndUpdate(songId, songToUpdate, { new: true });  
-      if (!updatedSong) {
-        return res.status(404).json({ message: 'Song not found' });
-      }  
-      res.status(200).json(updatedSong);
+        const songId = req.params.songId;
+        const songToUpdate = req.body;   
+        const updatedSong = await Songs.findByIdAndUpdate(songId, songToUpdate, { new: true });  
+        if (!updatedSong) {
+            return res.status(404).json({ message: 'Song not found' });
+        }  
+        res.status(200).json(updatedSong);
     } catch (error) {
-      res.status(500).json({ message: 'Error updating this Song' });
+        res.status(500).json({ message: 'Error updating this Song' });
     }
-  });
+});
 
-  // delete one Song
-  router.delete("/:songId", async (req, res) => {
+// delete one Song
+router.delete("/:songId", async (req, res) => {
     try {
         const songId = req.params.songId;
         const deletedSong = await Songs.findByIdAndDelete(songId);
         if (!deletedSong) {
-          return res.status(404).json({ message: "Song not found." });
+            return res.status(404).json({ message: "Song not found." });
         }
         res.status(200).send();
       } catch (error) {
         res.status(500).json({ message: 'Error deleting this Song' });
       }
-    });
+});
 
-    module.exports = router;
+module.exports = router;
