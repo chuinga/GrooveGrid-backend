1f408740c129c74c328913825ff6a508cd0622bb
“Carina <ksfraan@hotmail.com>
2024-01-26 16:20:29 +0000

    start songs sample and create user Route

---
+++ routes/user.routes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User.model');
+const { isAuthenticated } = require('../middleware/middleware');
+
+router.get('/:id', isAuthenticated, async (req, res) => {
+    try {
+        const oneUser = await User.findById(req.params.id);
+        res.json(oneUser);
+    } catch (error) {
+        next(error);
+    }
+});
+
+module.exports = router;
